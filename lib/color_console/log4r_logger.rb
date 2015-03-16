9d43d1be93a946f11ac012c98f9eaf280a4925f8
Adam Gardiner <adam.b.gardiner@gmail.com>
2015-03-16 11:54:23 +0000

    Fixes for Log4r outputter

--- lib/color_console/log4r_logger.rb
+++ lib/color_console/log4r_logger.rb
@@ -2,7 +2,7 @@ require 'log4r'
 require_relative '../color_console'
 
 
-# A module for using our color console for log output with the java.util.logging
+# A module for using our color console for log output with the Log4r
 # log framework.
 module Console
 
@@ -22,7 +22,7 @@ module Console
             def format(event)
                 @fg = case event.level
                 when Log4r::INFO then :white
-                when Log4r::WARN then :yellow
+                when Log4r::WARNING then :yellow
                 when Log4r::ERROR then :red
                 when Log4r::DEBUG then :dark_gray
                 else :light_gray
@@ -33,7 +33,7 @@ module Console
                         thread = Log4r::NDC.peek.to_s.upcase[0, 2]
                         level = Log4r::LNAMES[event.level]
                         case
-                        when event.data.is_a?(Exception) || event.data.java_kind_of?(java.lang.Throwable)
+                        when event.data.is_a?(Exception) || (RUBY_ENGINE == 'jruby' && event.data.java_kind_of?(java.lang.Throwable))
                             msg = Exception.format(event.data)
                         when event.data.is_a?(Array)
                             msg = event.data
@@ -49,10 +49,8 @@ module Console
                         end
                         msg
                     rescue Object => ex
-                        "(Unable to format event.data)" # due to #{ex})\n"
+                        "(Unable to format event.data due to #{ex})\n"
                     end
-                else
-                    ""
                 end
             end
 
@@ -67,7 +65,7 @@ module Console
                         end
                         Console.puts
                     end
-                else
+                elsif data
                     Console.puts(data, @fg)
                 end
             end
@@ -91,12 +89,10 @@ module Console
         level = case options.delete(:level)
         when String, Symbol then Log4r::LNAMES.index(options[:level].to_s.upcase)
         end
-        STDOUT.puts level
 
         # Remove any existing console handler
         log = logger ? Log4r::Logger[logger] : Log4r::Logger.root
         log = Log4r::Logger.new(logger) unless log
-
         log.outputters.each do |o|
             log.remove(o.name) if h.is_a?(Log4r::StdoutOutputter)
         end
